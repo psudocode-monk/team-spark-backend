0c829eb5ed0dd45dbeee5ca564c2027dde03be77
psudocode-monk <avisheksadhukhan7@gmail.com>
2025-10-09 20:56:54 +0530

    Merge with Reset Password

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -1,79 +1,3 @@
-<<<<<<< HEAD
-import bcrypt from 'bcryptjs'
-import jwt from 'jsonwebtoken'
-import User from '../models/user.models.js'
-
-export const registerUser = async (req, res) => {
-  const { name, email, password } = req.body
-  try {
-
-    // Check if user already exists
-
-    const existingUser = await User.findOne({ email })
-
-    if (existingUser) {
-      return res.status(400).send({ message: 'User already exists' })
-    }
-
-    // Hash the password
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    // Create a new user
-
-    const newUser = new User({ name, email, password: hashedPassword })
-    await newUser.save()
-
-    // Generate a JWT token
-
-    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
-
-    // Set token in HTTP-only cookie
-
-    res.cookie('token', token, { httpOnly: true })
-
-    res.status(201).send({ user: { id: newUser._id, name: newUser.name, email: newUser.email } })
-
-  } catch (error) {
-    res.status(500).send({ message: error.message })
-  }
-}
-
-export const loginUser = async (req, res) => {
-  const { email, password } = req.body
-  try {
-    // Find the user by email
-    const user = await User.findOne({ email })
-
-    if (!user) {
-      return res.status(400).send({ message: 'Invalid credentials' })
-    }
-
-    // Compare the password
-
-    const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) {
-      return res.status(400).send({ message: 'Invalid credentials' })
-    }
-
-    // Generate a JWT token
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' })
-
-    // Set token in HTTP-only cookie
-    res.cookie('token', token, { httpOnly: true })
-
-    res.status(200).send({ user: { id: user._id, name: user.name, email: user.email } })
-
-  } catch (error) {
-    res.status(500).send({ message: 'Server error' })
-  }
-}
-
-export const logoutUser = (req, res) => {
-  res.clearCookie('token')
-  res.status(200).send({ message: 'Logged out successfully' })
-=======
 import bcrypt from 'bcryptjs'
 import jwt from 'jsonwebtoken'
 import User from '../models/user.models.js'
@@ -249,6 +173,4 @@ export const resetPassword = async(req,res)=>{
         res.status(500).json({ message: "An internal server error occurred." });
         console.error(error);
     }
-
->>>>>>> 6e0319bb10720f2f1f48d962d5a2aa4c9d66acc9
-}
+  }
