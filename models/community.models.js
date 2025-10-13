9dff5a5fc89a5f8740b7370f6b545eec97182121
piyuscoder <piyushks4121@gmail.com>
2025-10-13 11:50:59 +0530

    Add files via upload

---
+++ models/community.models.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const communityPostSchema = new mongoose.Schema(
+  {
+    userIcon: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    likes: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    comments: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Comment",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+
+export default mongoose.model("CommunityPost", CommunityPost);
